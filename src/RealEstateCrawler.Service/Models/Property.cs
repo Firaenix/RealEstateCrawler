2db6d09f8e21ce3be8e5d9737d95e5f00604c881
Firaenix <Nick.Carton@gmail.com>
2016-07-04 00:08:51 +1000

    Renamed DomainCrawler to RealEstateCrawler

---
+++ src/RealEstateCrawler.Service/Models/Property.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstateCrawler.Service.Models
+{
+    public class Property
+    {
+        public Address Address;
+        public string Price;
+        public string PageUrl;
+        public string Bedrooms;
+        public string Bathrooms;
+        public string Parking;
+    }
+}
