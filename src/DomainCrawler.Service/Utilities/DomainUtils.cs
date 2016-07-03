e0f55239654cb9eb0c1aac0d8217a0169ce8d87d
Firaenix <Nick.Carton@gmail.com>
2016-07-04 00:09:29 +1000

    Renamed DomainCrawler to RealEstateCrawler

--- src/DomainCrawler.Service/Utilities/DomainUtils.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RealEstateCrawler.Service.Utilities
-{
-    public static class DomainUtils
-    {
-        public static string PropertyRegex = @"(?i)(?<Price>\$.+)\s*(?<No>(\d)+([A-Z])?(\/((\d)+([A-Z])?))?)\s*(?<Street>.+),\s*(?<Suburb>.+),\s*(?<State>.+)\s*(&n\s*bsp;)(?<PostCode>\w+)\s+(?<Bed>\w+)\s+beds\s+(?<Bath>\w+)\s+bathrooms\s+(?<Park>\w+)";
-        public static string LinkRegex = @"href='(?<Link>.+)'\s*data-listing-id";
-    }
-}
